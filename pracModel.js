c654017dc360a8566cb4acb91362120a14ea4db5
dalramjwi <dalramjwi@gmail.com>
2024-07-10 11:23:56 +0900

    body에 추가,return 추가

--- pracModel.js
+++ pracModel.js
@@ -4,44 +4,51 @@ function elementMaker(tagName, styles, textContent) {
     element.style[value] = styles[value];
   }
   element.textContent = textContent;
+  return element;
 }
 //tagname과 textContent는 string, styles는 배열로 이뤄져있는 객체
-const divs = ["root", "login-container", "logininput", "loginsubmit"];
+const divs = ["root", "loginContainer", "loginInput", "loginSubmit"];
 const styles = {
-  root: [
-    "position: relative",
-    "width: 100%",
-    "height: 100%",
-    "background: #ffffff",
-    "display: flex",
-    "flex-direction: column",
-    "justify-content: center",
-    "align-items: center",
-  ],
-  loginContainer: [
-    "position: relative",
-    "width: 556px",
-    "height: 748px",
-    "background: #666666",
-  ],
-  loginInput: [
-    "position: absolute",
-    "background: #d9d9d9",
-    "left: 12.95%",
-    "right: 13.13%",
-    "top: 38.24%",
-    "bottom: 50%",
-  ],
-  loginSubmit: [
-    "position: absolute",
-    "background: #9aff92",
-    "left: 44.96%;%",
-    "right: 13.13%",
-    "top : 66.84%",
-    "bottom: 21.39%",
-  ],
+  root: {
+    position: "relative",
+    width: "100%",
+    height: "100%",
+    background: "#ffffff",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  loginContainer: {
+    position: "relative",
+    width: "556px",
+    height: "748px",
+    background: "#666666",
+  },
+  loginInput: {
+    position: "absolute",
+    background: "#d9d9d9",
+    left: "12.95%",
+    right: "13.13%",
+    top: "38.24%",
+    bottom: "50%",
+  },
+  loginSubmit: {
+    position: "absolute",
+    background: "#9aff92",
+    left: "44.96%",
+    right: "13.13%",
+    top: "66.84%",
+    bottom: "21.39%",
+  },
 };
-const texts = ["ex"];
+
+const texts = [
+  "Root Element",
+  "Login Container",
+  "Login Input",
+  "Login Submit",
+];
 class DivObjMaker {
   constructor(tagName, styles, textContent) {
     this.tagName = tagName;
@@ -49,9 +56,22 @@ class DivObjMaker {
     this.textContent = textContent;
   }
 }
-const divMake = new DivObjMaker(divs, styles, "ex");
+const divMake = new DivObjMaker(divs, styles, texts);
 console.log(divMake);
 
-for (let i = 0; i < divs.length; i++) {
-  elementMaker(divs[i], styles, texts);
+for (let i = 0; i < divMake.tagName.length; i++) {
+  let styleKey = Object.keys(divMake.styles)[i];
+
+  appendToBody(
+    elementMaker(
+      divMake.tagName[i],
+      divMake.styles[styleKey],
+      divMake.textContent[i]
+    )
+  );
+}
+
+function appendToBody(element) {
+  document.body.appendChild(element);
+  //body에 한꺼번에 append 하기 때문에 모두 형제
 }
